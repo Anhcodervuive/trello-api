f0594ff3ec7951908975ca5264036ee24ecfb8f1
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-05-08 10:49:49 +0700

    Connect mongodb config env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,40 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+import { env } from '~/config/environment';
+
+const MONGODB_URL = env.MONGODB_URI
+const DATABASE_NAME = env.DATABASE_NAME;
+// Khởi tạo một đối tượng trelloDatabaseInstance là null (vì chúng ta chưa connect)
+let trelloDatabaseInstance = null;
+
+// Khởi tạo một đối tượng Client instance để connect tới mongodb
+const mongoClientInstance = new MongoClient(MONGODB_URL, {
+//  Lưu ý : cái serverApi có từ mongo > 5.0.0, có thể không cần dùng đến nó, còn nếu dùng nó là chúng ta sẽ
+//  chỉ định một cái stable api version của mongodb
+//  đọc thêm https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Kết nối tới csdl
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) {
+    throw new Error('Must connect to database first!');
+  } else {
+    return trelloDatabaseInstance;
+  }
+}
+
+export const CLOSE_DB = async () => {
+  console.log('Disconnecting from Mongodb Cloud Atlas...');
+  await mongoClientInstance.close();
+}
--- src/server.js
+++ src/server.js
@@ -1,26 +1,45 @@
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import express from 'express';
+import exitHook from 'async-exit-hook';
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
+import { mapOrder } from '~/utils/sorts.js';
+import { env } from './config/environment';
 
-const app = express()
+const START_SERVER = () => {
+  const app = express();
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = env.APP_HOST;
+  const port = env.APP_PORT;
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    // Test Absolute import mapOrder
+    console.log(mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    ))
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello, I am running at http://${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello, I am running at http://${ hostname }:${ port }/`);
+  })
+
+  exitHook(() => {
+    CLOSE_DB();
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to Mongodb Cloud Atlas!'))
+  .then(() => START_SERVER())
+  .catch(err => {
+    console.log(err);
+    process.exit(0);
+  })
