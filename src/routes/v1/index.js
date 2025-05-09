dffed936263975319c4f01812ae4988c75647675
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-05-09 13:49:53 +0700

    Generate an example routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express';
+import { StatusCodes } from 'http-status-codes'
+
+import { BoardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) =>
+  res.status(StatusCodes.OK).json({
+    message: 'api-v1'
+  })
+)
+Router.use('/boards', BoardRoutes);
+
+export const APIs_v1 = Router;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import exitHook from 'async-exit-hook';
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
-import { mapOrder } from '~/utils/sorts.js';
 import { env } from './config/environment';
+import { APIs_v1 } from './routes/v1';
 
 const START_SERVER = () => {
   const app = express();
@@ -10,21 +10,7 @@ const START_SERVER = () => {
   const hostname = env.APP_HOST;
   const port = env.APP_PORT;
 
-  app.get('/', async (req, res) => {
-    // Test Absolute import mapOrder
-    console.log(mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    ))
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_v1)
 
   app.listen(port, hostname, () => {
     // eslint-disable-next-line no-console
