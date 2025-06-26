e5b63b562319009cfb64a8a347ee052067e11678
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-06-26 17:44:55 +0700

    Logout, refresh token feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import ms from 'ms';
 
 import { userService } from '~/services/userService';
+import ApiError from '~/utils/ApiError';
 
 const createNew = async (req, res, next) => {
   try {
@@ -53,8 +54,39 @@ const login = async (req, res, next) => {
   }
 };
 
+const logout = async (req, res, next) => {
+  try {
+    // Xóa cookie – đơn giản là làm ngược lại so với việc gán cookie ở hàm login
+    res.clearCookie('accessToken')
+    res.clearCookie('refreshToken')
+
+    res.status(StatusCodes.OK).json({ loggedOut: true })
+  } catch (error) {
+    next(error)
+  }
+}
+
+const refreshToken = async (req, res, next) => {
+  try {
+    const accessToken = await userService.refreshToken(req.cookies?.refreshToken)
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    })
+
+    res.status(StatusCodes.OK).json(accessToken)
+  } catch (error) {
+    next(new ApiError(StatusCodes.FORBIDDEN, 'Please Sign In! (Error from refresh Token)'))
+  }
+}
+
+
 export const userController = {
   createNew,
   verifyAccount,
-  login
+  login,
+  logout,
+  refreshToken
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -123,8 +123,30 @@ const login = async (reqBody) => {
   }
 }
 
+const refreshToken = async (clientRefreshToken) => {
+  try {
+    const refreshTokenDecoded = await JwtProvider.verifyToken(clientRefreshToken, env.REFRESH_TOKEN_SECRET_SIGNATURE)
+
+    const userInfo = {
+      _id: refreshTokenDecoded._id,
+      email: refreshTokenDecoded.email
+    }
+
+    const accessToken = await JwtProvider.generateToken(userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      env.ACCESS_TOKEN_LIFE
+    )
+
+    return accessToken
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+}
+
 export const userService = {
   createNew,
   verifyAccount,
-  login
+  login,
+  refreshToken
 }
